84cd14baa1be95e1328634dd958865a8dc223c10
annna5 <annna.gawlowska@gmail.com>
2015-06-05 21:57:43 +0200

    modules created

---
+++ devices/gates.py
@@ -0,0 +1,281 @@
+""" Modeule contains all Devices types """
+import pygame
+import os, inspect, sys
+from pygame.sprite import Sprite
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)	
+class Device(Sprite):
+    """ Parent device class """
+    ICON_W, ICON_H = 100, 63
+
+    def __init__(self, simulator):
+        Sprite.__init__(self)
+        self.simulator = simulator
+        self.state = 0
+        wires = []
+        wires_ended = []
+        wires_started = []
+        self.wires = {'wires' : wires, 'wires_ended' :wires_ended, 'wires_started' :wires_started}
+        self.rects = {'icon_rect' : None, 'move_rect' : None}
+        self.icon = None
+        self.rect = None
+
+    def draw(self):
+        """ Draw device icon """
+        self.simulator.vis.screen.blit(self.icon, self.rects['icon_rect'])
+        pygame.display.flip()
+        self.update()
+
+    def icon_init(self, name):
+        """ Initialize device icon """
+        self.icon = pygame.image.load(os.path.join(os.path.dirname(cmd_folder), "visualizers/icons/"+name+".jpg")).convert()
+        self.rects['icon_rect'] = self.icon.get_rect()
+        self.rect = self.rects['icon_rect']
+        self.rects['move_rect'] = pygame.Rect(self.rect.left, self.rect.top, 15, 15)
+
+class Gate(Device):
+    """ Class represents all gates types """
+    def __init__(self, simulator):
+        Device.__init__(self, simulator)
+        self.inputs_max = 2    # poza not
+        self.inputs = []
+        self.outputs = []
+
+    def remove_from_group(self):
+        """ Remove gate from sprite group """
+        self.simulator.devices['gates'].remove(self)
+
+    def add_to_group(self):
+        """ Add gate to sprite group """
+        self.simulator.devices['gates'].add(self)
+
+    def wire_begin_pos(self):
+        """ Calculate posistion where wire starts """
+        return (self.rect.right, int(self.rect.top + self.ICON_H / 2))
+
+    def wire_end_pos(self):
+        """ Calculate posistion where wire ends """
+        return [(self.rect.left, int(self.rect.top + self.ICON_H / 3.)-8),
+                (self.rect.left, int(self.rect.top + self.ICON_H)-14)]
+
+    def add_started_wire(self, wire):
+        """ Add wire to gate lists """
+        self.wires['wires_started'].append(wire)
+        self.wires['wires'].append(wire)
+
+    def add_ended_wire(self, wire):
+        """ Add wire to gate lists """
+        self.wires['wires_ended'].append(wire)
+        self.wires['wires'].append(wire)
+
+    def remove_wires(self):
+        """ Clear wires lists after removing device """
+        self.wires['wires'] = []
+        self.wires['wires_ended'] = []
+        self.wires['wires_started'] = []
+
+class GateAnd(Gate):
+    """ Gate represents logical conjunction """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("and")
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 1:
+            if self.inputs[0].state == 1 and self.inputs[1].state == 1:
+                self.state = 1
+            else:
+                self.state = 0
+
+class GateOr(Gate):
+    """ Gate represents logical disjunction """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("or")
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 1:
+            if self.inputs[0].state == 0 and self.inputs[1].state == 0:
+                self.state = 0
+            else:
+                self.state = 1
+
+class GateNot(Gate):
+    """ Gate represents logical Negation """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("not")
+        self.inputs_max = 1
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 0:
+            self.state = (self.inputs[0].state +1) % 2
+
+    def wire_end_pos(self):
+        return [(self.rect.left, int(self.rect.top + self.ICON_H / 2)-1)]
+        
+class GateBuffor(Gate):
+    """ Buffor """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("buffor")
+        self.inputs_max = 1
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 0:
+            self.state = (self.inputs[0].state)
+
+    def wire_end_pos(self):
+        return [(self.rect.left, int(self.rect.top + self.ICON_H / 2)-1)]        
+
+class GateNand(Gate):
+    """ Gate represents nand operation """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("nand")
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 1:
+            if self.inputs[0].state == 1 and self.inputs[1].state == 1:
+                self.state = 0
+            else:
+                self.state = 1
+
+class GateNor(Gate):
+    """ Gate represents logical nor """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("nor")
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 1:
+            if self.inputs[0].state == 0 and self.inputs[1].state == 0:
+                self.state = 1
+            else:
+                self.state = 0
+
+class GateXor(Gate):
+    """ Gate represents logical exclusive or """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("xor")
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 1:
+            if self.inputs[0].state == self.inputs[1].state:
+                self.state = 0
+            else:
+                self.state = 1
+
+class Bulb(Device):
+    """ Represents circuit output """
+    def __init__(self, simulator):
+        Device.__init__(self, simulator)
+        self.icon_init("bulb0")
+        self.icon0 = pygame.image.load(os.path.join(os.path.dirname(cmd_folder), "visualizers/icons/bulb0.jpg")).convert()
+        self.icon1 = pygame.image.load(os.path.join(os.path.dirname(cmd_folder), "visualizers/icons/bulb1.jpg")).convert()
+        self.inputs = []
+        self.inputs_max = 1
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 0:
+            self.state = self.inputs[0].state
+        if self.state == 1:
+            self.icon = self.icon1
+        else:
+            self.icon = self.icon0
+
+    def remove_from_group(self):
+        """ Removes bulb from its sprite group """
+        self.simulator.devices['bulbs'].remove(self)
+
+    def add_to_group(self):
+        """ Add bulb to its sprite group """
+        self.simulator.devices['bulbs'].add(self)
+
+    def wire_end_pos(self):
+        """ Calculate posistion where wire ends """
+        return [(self.rect.left, self.rect.top + int(self.ICON_H / 2))]
+
+    def add_ended_wire(self, wire):
+        """ Add wire to gate lists """
+        self.wires['wires_ended'].append(wire)
+        self.wires['wires'].append(wire)
+
+    def remove_wires(self):
+        """ Clear wires lists after removing bulb """
+        self.wires['wires'] = []
+        self.wires['wires_ended'] = []
+
+class Switch(Device):
+    """ Represents circuit input """
+    def __init__(self, simulator):
+        Device.__init__(self, simulator)
+        self.icon_init("switch0")
+        self.icon0 = pygame.image.load(os.path.join(os.path.dirname(cmd_folder), "visualizers/icons/switch0.jpg")).convert()
+        self.icon1 = pygame.image.load(os.path.join(os.path.dirname(cmd_folder), "visualizers/icons/switch1.jpg")).convert()
+        self.outputs = []
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if self.state == 1:
+            self.icon_init("switch1")
+            self.rects['icon_rect'] = self.icon.get_rect()
+            self.rect = self.rects['icon_rect']
+
+    def remove_from_group(self):
+        """ Removes switch from its sprite group """
+        self.simulator.devices['switches'].remove(self)
+
+    def add_to_group(self):
+        """ Add bulb to its sprite group """
+        self.simulator.devices['switches'].add(self)
+
+    def wire_begin_pos(self):
+        """ Calculate posistion where wire starts """
+        return (self.rect.right, int(self.rect.top + self.ICON_H / 2) - 1)
+
+    def add_started_wire(self, wire):
+        """ Add wire to gate lists """
+        self.wires['wires_started'].append(wire)
+        self.wires['wires'].append(wire)
+
+    def remove_wires(self):
+        """ Clear wires lists after removing switch """
+        self.wires['wires'] = []
+        self.wires['wires_started'] = []
+        
+class Knot(Gate):
+    """ Gate represents logical Negation """
+    def __init__(self, simulator):
+        Gate.__init__(self, simulator)
+        self.icon_init("knot")
+        self.inputs_max = 1
+
+    def update(self):
+        """ Updates gate state """
+        self.state = 0
+        if len(self.inputs) > 0:
+            self.state = (self.inputs[0].state)
+
+    def wire_end_pos(self):
+        return [(self.rect.left, int(self.rect.top + self.ICON_H / 2)-1)]
+
---
+++ visualizers/visualizers.py
@@ -0,0 +1,158 @@
+""" Module with class Visualizer"""
+import pygame
+import os, inspect, sys
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)	
+
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"icons")))
+if cmd_subfolder not in sys.path:
+	sys.path.insert(0, cmd_subfolder)    
+
+class Visualizer(object):
+    """ Class containng drawing methods"""
+    SCREEN_W, SCREEN_H = 1300, 700
+    ICON_W, ICON_H = 100, 63
+    ICONS = ['and.jpg', 'or.jpg', 'nand.jpg', 'nor.jpg', 'buffor.jpg', 'not.jpg', 'xor.jpg', 'switch0.jpg', 'bulb0.jpg', 'knot.jpg']
+    BLACK, BLUE, WHITE, BLUE2 = (0, 0, 0), (83, 126, 139), (255, 255, 255), (184, 225, 227)
+    GRAY, GRAY2, GRAY3 = (64, 64, 64), (54, 54, 54), (32, 32, 32)
+    H_MARGIN = 30
+    V_MARGIN = 20
+    SPACE = 14
+    BUTTON_W, BUTTON_H = 116, 40
+   
+
+
+    def __init__(self, simulator):
+        self.draw_st_panel = True
+        self.screen = pygame.display.set_mode((self.SCREEN_W, self.SCREEN_H))
+        self.simulator = simulator
+
+        main_panel_height = self.SCREEN_H - 2 * self.H_MARGIN
+        left_panel_width = self.ICON_W + self.SPACE
+        left_panel = pygame.Rect(self.V_MARGIN, self.H_MARGIN -4, left_panel_width, main_panel_height)
+
+        main_panel_width = self.SCREEN_W  - left_panel_width - self.BUTTON_W - self.V_MARGIN * 4
+        main_panel = pygame.Rect(left_panel_width + self.V_MARGIN * 2,\
+                                 self.H_MARGIN, main_panel_width, main_panel_height)
+        inside_main_panel = pygame.Rect(main_panel.left + self.ICON_W / 2 + 2,\
+                                        main_panel.top + self.ICON_H / 2 + 2,\
+                                        main_panel_width - self.ICON_W - 2,\
+                                        main_panel_height - self.ICON_H - 2)
+
+        devices_panel = pygame.Rect(self.V_MARGIN + self.SPACE / 2, self.H_MARGIN+4, self.ICON_W, self.ICON_H * len(self.ICONS))
+
+        help_button = pygame.Rect(self.SCREEN_W - self.V_MARGIN - self.BUTTON_W,\
+                                  self.H_MARGIN, self.BUTTON_W, self.BUTTON_H)
+        clear_button = pygame.Rect(self.SCREEN_W - self.V_MARGIN - self.BUTTON_W,\
+                                   self.SCREEN_H - self.H_MARGIN - self.BUTTON_H,\
+                                   self.BUTTON_W, self.BUTTON_H)
+        print_button = pygame.Rect(self.SCREEN_W - self.V_MARGIN - self.BUTTON_W,\
+                                   self.SCREEN_H - self.H_MARGIN - self.SPACE - self.BUTTON_H * 2,
+                                   self.BUTTON_W, self.BUTTON_H)
+
+        start_panel = pygame.Rect(left_panel_width + self.V_MARGIN * 2,\
+                                 self.H_MARGIN, main_panel_width, main_panel_height)
+        start_button = pygame.Rect(start_panel.left + main_panel_width/2-90,\
+                                   start_panel.bottom - 90, 180, 60)
+
+        self.simulator.panels = {'start_panel':start_panel, 'help_button':help_button,\
+                                 'start_button':start_button, 'main_panel' : main_panel,\
+                                 'inside_main_panel' : inside_main_panel, 'left_panel' : left_panel,\
+                                 'devices_panel' : devices_panel, 'clear_button' : clear_button,\
+                                 'print_button' : print_button}
+        self.images = []
+        for i in range(len(self.ICONS)):
+            #icon = pygame.image.load("icons/"+self.ICONS[i]).convert()
+            icon = pygame.image.load(os.path.join(cmd_folder,"icons/"+ self.ICONS[i])).convert()
+            self.images.append(icon)
+
+    def draw_background(self):
+        """ Draws window background """
+        tile_img = pygame.image.load(os.path.join(cmd_folder,"background/2.png")).convert()
+        img_rect = tile_img.get_rect()
+        nrows = int(self.SCREEN_H / img_rect.height) + 1
+        ncols = int(self.SCREEN_W  / img_rect.width) + 1
+
+        for row in range(nrows):
+            for col in range(ncols):
+                img_rect.topleft = (col * img_rect.width, row * img_rect.height)
+                self.screen.blit(tile_img, img_rect)
+
+        self.draw_rects()
+        self.draw_buttons()
+        self.draw_icons()
+        if self.draw_st_panel == True:
+            self.draw_start_panel()
+
+    def draw_rects(self):
+        """ Draws panels """
+        pygame.draw.rect(self.screen, self.BLACK, self.simulator.panels['left_panel'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['left_panel'], 4)
+        pygame.draw.rect(self.screen, self.BLACK, self.simulator.panels['main_panel'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['main_panel'], 4)
+
+    def draw_buttons(self):
+        """ Draws buttons """
+        pygame.draw.rect(self.screen, self.GRAY2, self.simulator.panels['clear_button'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['clear_button'], 4)
+        pygame.draw.rect(self.screen, self.GRAY2, self.simulator.panels['print_button'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['print_button'], 4)
+        pygame.draw.rect(self.screen, self.GRAY2, self.simulator.panels['help_button'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['help_button'], 4)
+        myfont = pygame.font.SysFont("monospace", 30)
+        myfont.set_bold(True)
+
+        clear_button_text = pygame.Rect(self.simulator.panels['clear_button'].left + 4,\
+                                        self.simulator.panels['clear_button'].top + 4,\
+                                        self.BUTTON_W, self.BUTTON_H)
+        clear_button_msg = myfont.render("CLEAR", 1, self.BLACK)
+        self.screen.blit(clear_button_msg, clear_button_text)
+
+        print_button_text = pygame.Rect(self.simulator.panels['print_button'].left + 4,\
+                                        self.simulator.panels['print_button'].top + 4,\
+                                        self.BUTTON_W, self.BUTTON_H)
+        print_button_msg = myfont.render("PRINT", 1, self.BLACK)
+        self.screen.blit(print_button_msg, print_button_text)
+
+        help_button_text = pygame.Rect(self.simulator.panels['help_button'].left + 14,\
+                                       self.simulator.panels['help_button'].top + 4,\
+                                       self.BUTTON_W, self.BUTTON_H)
+        help_button_msg = myfont.render("HELP", 1, self.BLACK)
+        self.screen.blit(help_button_msg, help_button_text)
+
+    def draw_icons(self):
+        """ Draws left panel """
+        for i in range(len(self.ICONS)):
+            img_rect = self.images[i].get_rect()
+            img_rect.topleft = (self.V_MARGIN + self.SPACE / 2, self.simulator.panels['devices_panel'].top+i * img_rect.height)
+            self.screen.blit(self.images[i], img_rect)
+        for gate in self.simulator.devices['gates']:
+            gate.update()
+            self.screen.blit(gate.icon, gate.rects['icon_rect'])
+        for switch in self.simulator.devices['switches']:
+            self.screen.blit(switch.icon, switch.rects['icon_rect'])
+        for bulb in self.simulator.devices['bulbs']:
+            bulb.update()
+            self.screen.blit(bulb.icon, bulb.rects['icon_rect'])
+        for wire in self.simulator.devices['wires']:
+            wire.draw(self.screen)
+
+    def draw_start_panel(self):
+        """ Draws start (help) window """
+        myfont = pygame.font.SysFont("monospace", 48)
+        myfont.set_bold(True)
+        pygame.draw.rect(self.screen, self.BLACK, self.simulator.panels['start_panel'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['start_panel'], 4)
+        pygame.draw.rect(self.screen, self.GRAY2, self.simulator.panels['start_button'])
+        pygame.draw.rect(self.screen, self.GRAY3, self.simulator.panels['start_button'], 4)
+        start_button_text = pygame.Rect(self.simulator.panels['start_button'].left + 6,\
+                                        self.simulator.panels['start_button'].top + 6, 100, 80)
+        start_button_msg = myfont.render("START", 1, self.BLACK)
+        self.screen.blit(start_button_msg, start_button_text)
+        help_img = pygame.image.load(os.path.join(cmd_folder,"background/help.png")).convert()
+        img_rect = help_img.get_rect()
+        img_rect.center = self.simulator.panels['start_panel'].center
+        self.screen.blit(help_img, img_rect)
+
